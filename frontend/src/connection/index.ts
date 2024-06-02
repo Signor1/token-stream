488f05135156ab3de1b53927dff7ed94e1a41d62
signor1 <emmanuelomemgboji@gmail.com>
2024-06-02 21:43:11 +0100

    feat: updated network info

--- frontend/src/connection/index.ts
+++ frontend/src/connection/index.ts
@@ -1,12 +1,12 @@
 import { createWeb3Modal, defaultConfig } from "@web3modal/ethers/react";
 
-export const SUPPORTED_CHAIN_ID = 11155111;
+export const SUPPORTED_CHAIN_ID = 80002;
 
-const sepolia = {
-  chainId: 11155111,
-  name: "Sepolia",
-  currency: "ETH",
-  explorerUrl: "https://etherscan.io",
+const polygonamoy = {
+  chainId: 80002,
+  name: "Polygon Amoy Testnet",
+  currency: "MATIC",
+  explorerUrl: "https://amoy.polygonscan.com/",
   rpcUrl: import.meta.env.VITE_RPC_URL,
 };
 
@@ -20,7 +20,7 @@ const metadata = {
 export const configWeb3Modal = () =>
   createWeb3Modal({
     ethersConfig: defaultConfig({ metadata }),
-    chains: [sepolia],
+    chains: [polygonamoy],
     projectId: import.meta.env.VITE_PROJECT_ID,
     enableAnalytics: false, // Optional - defaults to your Cloud configuration
   });
